3d6a142e81ec7f885fe84b8ea520b216f244ea7c
Anthony Crawford <anthonycrawford2@mac.com>
2023-05-01 13:27:18 -0500

    enviornmental variable EXPERT_TEAM_ID miss spelling

--- server/database/postgres/queries/personnel/query/expert-connect.js
+++ server/database/postgres/queries/personnel/query/expert-connect.js
@@ -2,7 +2,7 @@ import { client, pool } from "../../../connection.js";
 import moment from "moment";
 
 export const sqlQueryExpertConnectTeamByDeviceName = async (deviceName) => {
-  let team = { rows: [{ ec_id: process.env.EXPRESS_TEAM_ID }] };
+  let team = { rows: [{ ec_id: process.env.EXPERT_TEAM_ID }] };
   const rxp = new RegExp(/[0-9]+/gi);
 
   try {
@@ -11,7 +11,7 @@ export const sqlQueryExpertConnectTeamByDeviceName = async (deviceName) => {
     if (!device_number)
       throw new Error("Number designation wasn't found in device name");
 
-    team = await client.query(
+    const result = await client.query(
       `
           Select 
             ec_id,
@@ -23,10 +23,13 @@ export const sqlQueryExpertConnectTeamByDeviceName = async (deviceName) => {
       `,
       []
     );
+    if (!result.rows.length) throw new Error("Vehicle not assigned a team");
+    else team = result;
   } catch (e) {
     console.log("EXPERT CONNECT TEAM SELECT BY DEVICE NAME ERROR", e.message);
     console.log("EXPERT CONNECT TEAM SELECT BY DEVICE NAME STACK", e.stack);
   } finally {
+    console.log(team.rows);
     return team.rows[0].ec_id;
   }
 };
