f07bb54c650b2ba5783ff2fbbfb099fa66a36522
Anthony Crawford <anthonycrawford2@mac.com>
2023-08-26 10:26:48 -0500

    Fixed id column on bag-monitoring table

--- app/src/app/pages/dashboard/tabs/bag-monitoring/bag-monitoring.component.ts
+++ app/src/app/pages/dashboard/tabs/bag-monitoring/bag-monitoring.component.ts
@@ -20,7 +20,7 @@ export class BagMonitoringComponent implements OnInit, OnDestroy{
     
   // Public
   isLoading:boolean = true
-  currentOffset:number = 1
+  currentOffset:number = 0
   ticketsList = new MatTableDataSource([]);
   ticketsQuery: Subscription | undefined
   currentPageSize: any = new FormControl("10")
@@ -61,7 +61,7 @@ export class BagMonitoringComponent implements OnInit, OnDestroy{
 
   private requestTickets (){
     this.isLoading = true
-    this.currentOffset = 1
+    this.currentOffset = 0
     this.ticketService.getBagMonitoringTickets ({
       offset:this.currentOffset,
       limit:parseInt(this.currentPageSize.value),
@@ -131,7 +131,7 @@ export class BagMonitoringComponent implements OnInit, OnDestroy{
 
   resetResubmit(e: Event){
     console.log(e)
-    this.currentOffset = 1
+    this.currentOffset = 0
     this.requestTickets()
   }
 }
