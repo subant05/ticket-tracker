88ca4fb71ca08dbed6433cddda5dc0ae5a377514
subant05 <anthonycrawford2.com>
2025-04-11 21:57:40 -0500

    fixed issue

--- app/src/environments/shared.confg.ts
+++ app/src/environments/shared.confg.ts
@@ -1,29 +1,32 @@
-const sharedConfig =  {
-    googleAuthClientId:'1068443878218-kk375e4311cc3r2rk3b0hn9ukm900tjo.apps.googleusercontent.com',
-    mapboxAPIKey:"pk.eyJ1IjoiYW50Ymx1ZXJpdmVydGVjaCIsImEiOiJja295a3IwemowbjMwMndwZ2RkdHY2bmJjIn0.xpY0kRQNFTeFgc5l6hqRtQ",
-    serviceEndpoint:"/graphql",
-    requestToken:
-    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.MAtt7IgMfvx0f9pPkez_vMfxAcylTD4w0OC0H_V6cnE',
-    enpoints: {
-        post: {
-            rules: "/api/rules/formant"
-        },
-        put: {
-            rules: "/api/rules/formant"
-        },
-        delete: {
-            rules: "/api/rules/formant"
-        },
-        get: {
-            tickets:{ 
-                all:"/api/tickets/all",
-                bagMonitoring:"/api/tickets/bag-monitoring"
-            },
-            rules:{
-                all:"/api/rules/formant"
-            }
-        }
-    },
-}
+dotenv.config();
+import { config } from "dotenv";
+process.env.graphql_token;
 
-export {sharedConfig as default}
+const sharedConfig = {
+  googleAuthClientId: "",
+  mapboxAPIKey: "",
+  serviceEndpoint: "/graphql",
+  requestToken: `Bearer ${process.env.graphql_token}`,
+  enpoints: {
+    post: {
+      rules: "/api/rules/formant",
+    },
+    put: {
+      rules: "/api/rules/formant",
+    },
+    delete: {
+      rules: "/api/rules/formant",
+    },
+    get: {
+      tickets: {
+        all: "/api/tickets/all",
+        bagMonitoring: "/api/tickets/bag-monitoring",
+      },
+      rules: {
+        all: "/api/rules/formant",
+      },
+    },
+  },
+};
+
+export { sharedConfig as default };
